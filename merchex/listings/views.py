feb69737784df970d6806bd65816c3e06110ae2a
BioKobena <biokobena07@gmail.com>
2023-05-02 11:50:51 -0700

    Models creation

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,15 +4,55 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
+
 
 def hello(request):
-    return HttpResponse("<h1>Hello Django</h1>.")
+    bands = Band.objects.all()
+    return HttpResponse(
+        f""" 
+    <h1>Hello World!</h1>
+    <p>Mes groupes préférés sont : </p>
+    <ul> 
+        <li> 
+         Name : {bands[0].name}
+        </li> 
+     <li> 
+         Name : {bands[1].name}
+        </li> 
+         <li> 
+         Name : {bands[2].name}
+        </li> 
+    </ul> 
+    """
+    )
+
 
 def about(request):
-    return HttpResponse("<h1>About Django</h1>.")
+    lists = Listing.objects.all()
+    return HttpResponse(
+        f""" 
+    <h1>Hello World!</h1>
+    <p>Mes titres préférés sont : </p>
+    <ul> 
+        <li> 
+         Title : {lists[0].title}
+        </li> 
+     <li> 
+         Title : {lists[1].title}
+        </li> 
+         <li> 
+         Title : {lists[2].title}
+        </li> 
+    </ul> 
+    """
+    )
+
 
 def phone(request):
     return HttpResponse("<h1>Phone Django</h1>.")
 
+
 def footer(request):
-    return HttpResponse('<h1>Phone Django</h1>')
+    return HttpResponse("<h1>Phone Django</h1>")
