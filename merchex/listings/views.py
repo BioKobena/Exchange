a2cfd3eadb88ab939f7df1162e586295343ec684
BioKobena <biokobena07@gmail.com>
2023-05-03 08:48:56 -0700

    Les templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -10,48 +10,15 @@ from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(
-        f""" 
-    <h1>Hello World!</h1>
-    <p>Mes groupes préférés sont : </p>
-    <ul> 
-        <li> 
-         Name : {bands[0].name}
-        </li> 
-     <li> 
-         Name : {bands[1].name}
-        </li> 
-         <li> 
-         Name : {bands[2].name}
-        </li> 
-    </ul> 
-    """
-    )
+    return render(request, "listings/hello.html", {"bands": bands})
 
 
 def about(request):
-    lists = Listing.objects.all()
-    return HttpResponse(
-        f""" 
-    <h1>Hello World!</h1>
-    <p>Mes titres préférés sont : </p>
-    <ul> 
-        <li> 
-         Title : {lists[0].title}
-        </li> 
-     <li> 
-         Title : {lists[1].title}
-        </li> 
-         <li> 
-         Title : {lists[2].title}
-        </li> 
-    </ul> 
-    """
-    )
+    return render(request, "listings/about.html")
 
 
 def phone(request):
-    return HttpResponse("<h1>Phone Django</h1>.")
+    return render(request, "listings/contact.html")
 
 
 def footer(request):
