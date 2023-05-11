ffd1a1d8342d23d907d5c81f27aa5c1a051e358e
BioKobena <biokobena07@gmail.com>
2023-05-11 10:19:11 +0000

    New information

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -8,6 +8,7 @@ class Band(models.Model):
         HIP_HOP = "HH"
         SYNTH_POP = "SP"
         ALTERNATIVE_ROCK = "AR"
+        CHRETIENS = "CR"
 
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
